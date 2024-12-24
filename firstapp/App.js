7a264bc97d76c7aac84ed24abb6463925ddf78f5
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2024-12-24 14:40:35 +0000

    Used the divider component

--- firstapp/App.js
+++ firstapp/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { PaperProvider, Text } from 'react-native-paper';
+import {PaperProvider, Text, Divider } from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
         <Text variant="headlineLarge">Headline Large</Text>
+        <Divider/>
         <Text variant="bodyMedium" style={styles.body}>
         Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.
 To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.
